18fbf59927e72c0789146a4f9a1ce32ffca367dd
blackgerman <ulrichabiguime@gmail.com>
2022-11-15 12:59:39 +0000

    sc

--- lib/src/xrint.dart
+++ lib/src/xrint.dart
@@ -1,12 +1,9 @@
-
-
 import 'package:flutter/cupertino.dart';
 
-const bool XRINT_DEBUG_VALUE = true
-;
+const bool XRINT_DEBUG_VALUE = false;
 
-xrint (dynamic message,{bool debug = XRINT_DEBUG_VALUE}){
-  if (debug){
+xrint(dynamic message, {bool debug = XRINT_DEBUG_VALUE}) {
+  if (debug) {
     debugPrint("XXX ${message}");
   }
 }
