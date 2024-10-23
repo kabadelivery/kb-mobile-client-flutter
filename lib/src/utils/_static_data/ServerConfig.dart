a096862eb35cad6082e16be721aff398f287e99c
ulrich.dev <ulrichabiguime@gmail.com>
2024-10-23 08:04:16 -0400

    fix topic name

--- lib/src/utils/_static_data/ServerConfig.dart
+++ lib/src/utils/_static_data/ServerConfig.dart
@@ -18,11 +18,11 @@ class ServerConfig {
   static const String IMAGE_BUCKET_BASE_LINK =
       "https://kaba-delivery-pictures-store.s3.eu-west-3.amazonaws.com";
 
-  static String TOPIC = "kaba_flutter";
-  static String HMS_TOPIC = "kaba_flutter";
+//   static String TOPIC = "kaba_flutter";
+//   static String HMS_TOPIC = "kaba_flutter";
 
-  // static String TOPIC = "kaba_delivery_all";
-  // static String HMS_TOPIC = "kaba_delivery_all";
+  static String TOPIC = "kaba_delivery_all";
+  static String HMS_TOPIC = "kaba_delivery_all";
 
   static String LOGIN_EXPIRATION = "_login_expiration_date";
 
