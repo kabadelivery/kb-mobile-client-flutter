23e8b7f108560df53ca432ef9fd0d4c2b5437226
Firix00 <boonjoseph16@gmail.com>
2024-11-06 16:01:06 +0000

    commit changes

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/abiguime/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/abiguime/devspace/github/kb-mobile-client-flutter"
+export "FLUTTER_ROOT=/Users/joseph/Documents/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/joseph/Documents/GitHub/kb-mobile-client-flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
@@ -10,4 +10,4 @@ export "FLUTTER_BUILD_NUMBER=3.6.3"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=true"
-export "PACKAGE_CONFIG=/Users/abiguime/devspace/github/kb-mobile-client-flutter/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/joseph/Documents/GitHub/kb-mobile-client-flutter/.dart_tool/package_config.json"
