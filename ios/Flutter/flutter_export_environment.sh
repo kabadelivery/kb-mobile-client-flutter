abe70db769210a3d0aff48838d0018a9741634eb
Ulrich Abiguime <ulrichabiguime@gmail.com>
2025-04-19 11:04:49 -0400

    fix pod

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\lestu\kb-mobile-client-flutter"
+export "FLUTTER_ROOT=/Users/abiguime/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/abiguime/devspace/github/kb-mobile-client-flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=3.6.3"
 export "FLUTTER_BUILD_NUMBER=3.6.3"
