cf092387fd6cc8c58e2e069d2f45da907c2fc437
Firix00 <boonjoseph16@gmail.com>
2025-10-16 23:12:23 +0000

    Integrate user subscription details into order flow
    - Fetch user subscription information before computing billing and launching orders.
    - Include subscription data in the request payload for both standard and out-of-app orders.
    - Add new server endpoints for fetching user subscription details and saving subscription-related orders.
    - Refactor API calls in `order_api_provider.dart` and `out_of_app_order_api.dart` to handle new subscription logic and improve error handling.
    - Fix an issue in the expedition detail form to ensure cost estimation is recalculated when departure or arrival towns are changed.
    - Correct a typo in a server route configuration.

--- lib/src/utils/_static_data/ServerRoutes.dart
+++ lib/src/utils/_static_data/ServerRoutes.dart
@@ -352,13 +352,15 @@ class ServerRoutes {
   static const String KABA_ABONNEMENT_SUSCRIBED_USER = ServerConfig.kaba_abonnement + "/dashboard/subscribeduser";
   static const String KABA_UPDATE_PAYMENT_STATUS = ServerConfig.kaba_abonnement + "/dashboard/update_abo";
   static const String KABA_ABONNEMENT_SHARED_CODE = ServerConfig.kaba_abonnement + "/dashboard/subscribeduser";
+  static const String KABA_ABONNEMENT_GET_BY_USER = ServerConfig.kaba_abonnement + "dashboard/currentUserAbodetails";
+  static const String KABA_ABONNEMENT_SAVE_USER_ORDER = ServerConfig.kaba_abonnement + "/dashboard/saveUserOrderwithAbo";
   static const String ABO_CHECK_PAYMENT= ServerConfig.PAY_SERVER_ADDRESS_SECURE + "/api/check/subscription";
 
   static const String KABA_CHECK_ABO_PAYMENT_STATUS = ServerConfig.PAY_SERVER_ADDRESS_SECURE + "/api/subscription/transaction/check";
   static const String LINK_GET_DELIVERY_RATING_PENDING =
       ServerConfig.STAT_SERVER_ADDRESS +"/mobile/api/command/delivery_rating_pending/v4/get";
   static const String LINK_SEND_ORDER_FEEDBACK =ServerConfig.STAT_SERVER_ADDRESS + "/mobile/api/command/v4/rate";
-  static const String LINK_SUBSCRIBE_BY_CODE = ServerConfig.kaba_expedition + "/dashboard/sharedCodeSuscriber";
+  static const String LINK_SUBSCRIBE_BY_CODE = ServerConfig.kaba_abonnement + "/dashboard/sharedCodeSuscriber";
   static const String LINK_GET_SKIP_NOTATION_STATUS = ServerConfig.SERVER_ADDRESS + "/mobile/api/notation-skip/status/get";
   static const String LINK_GET_PERFORMANCE_RATING =ServerConfig.STAT_SERVER_ADDRESS + "/mobile/api/command/rating/v4/get";
   static const String LINK_PAY_BY_WALLET = ServerConfig.PAY_SERVER_ADDRESS_SECURE+"/api/subscription/pay/wallet";
