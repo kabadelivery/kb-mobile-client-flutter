0b30fdbfa245cbf18437a0de68df7d834a83b753
Firix00 <boonjoseph16@gmail.com>
2025-11-13 11:51:34 +0000

    Fix: Correct subscription order handling and update environment
    - Ensure subscription order data is saved only when it exists
    - Switch server configuration from production to development
    - Update application version from 3.8.0 to 3.8.3

--- lib/src/resources/order_api_provider.dart
+++ lib/src/resources/order_api_provider.dart
@@ -229,17 +229,19 @@ class OrderApiProvider {
       xrint("Response data: ${response.data}");
       xrint("Status code: ${response.statusCode}");
       if (response.statusCode == 200) {
-        try{
-          await dio.post(
-            Uri.parse(ServerRoutes.KABA_ABONNEMENT_SAVE_USER_ORDER).toString(),
-            data: {
-              'user_id':customer.id.toString(),
-              'subscription_id':abonnementData['pack']['id'].toString(),
-              'codeAbo':abonnementData['codeAbonnement'].toString(),
-              'command_id':mJsonDecode(response.data)['data']['command_id'].toString()
-            },
-          );
-        }catch(_){}
+        if(abonnementData.isNotEmpty){
+          try{
+            await dio.post(
+              Uri.parse(ServerRoutes.KABA_ABONNEMENT_SAVE_USER_ORDER).toString(),
+              data: {
+                'user_id':customer.id.toString(),
+                'subscription_id':abonnementData['pack']['id'].toString(),
+                'codeAbo':abonnementData['codeAbonnement'].toString(),
+                'command_id':mJsonDecode(response.data)['data']['command_id'].toString()
+              },
+            );
+          }catch(_){}
+        }
         return mJsonDecode(response.data);
       } else {
         throw Exception(-1); // erreur côté serveur
